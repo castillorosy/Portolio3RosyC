5ccb931b5c333d0114c2c8bc1c5c4b1b9eeb0f19
castillorosy <rosycastillo18@yahoo.com>
2015-01-07 14:02:45 -0800

    2nd commit

--- public_html/script.js
+++ public_html/script.js
@@ -1,7 +1,5 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$("document").ready(function(){
+    $(".test").css("background-color", "blue");
+    $('p:first').css({"background-color": "red", "color": "white"});
+  $('p:last').css({"background-color": "black", "color": "white"});
+});
