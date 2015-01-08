2a1af05eb6bbbd47d5f3bd16be671c4e3c02e258
castillorosy <rosycastillo18@yahoo.com>
2015-01-08 13:59:07 -0800

    3rd commit

--- public_html/script.js
+++ public_html/script.js
@@ -1,5 +1,15 @@
-$("document").ready(function(){
+$("document").ready(function() {
     $(".test").css("background-color", "blue");
     $('p:first').css({"background-color": "red", "color": "white"});
-  $('p:last').css({"background-color": "black", "color": "white"});
+    
+    $('p:last').css({"background-color": "black", "color": "white"});
+
 });
+
+$("window").resize(resizedWindow);
+
+function resizedWindow()
+{
+    $("#second").html("Window was resized W: " + $("window").width() + " H: + ");
+$(("window").height());
+}
