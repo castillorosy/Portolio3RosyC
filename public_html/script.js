b0003dc0fa2c7554a5fbf023de8f4e9918bed554
castillorosy <rosycastillo18@yahoo.com>
2015-01-09 13:59:53 -0800

    no message

--- public_html/script.js
+++ public_html/script.js
@@ -5,7 +5,22 @@ $("document").ready(function() {
     $('p:last').css({"background-color": "black", "color": "white"});
 
 });
+$('document').ready(function(){
+$("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);    
+$('div').bind('mouse', mouseOverMe);
+$('h1').bind('click', mouseClick);
+});
 
+function mouseOverMe() {
+    $("p").html("blah");
+}
+function mouseOutMe () {
+    $('h1').html("what's up?");
+}
+
+function mouseClick(){
+    $('p').html('ugh');
+}
 $("window").resize(resizedWindow);
 
 function resizedWindow()
