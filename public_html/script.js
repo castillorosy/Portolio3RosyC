74000b76e62af5dd2cc00bba59a5169fce412b5a
castillorosy <rosycastillo18@yahoo.com>
2015-01-12 14:18:59 -0800

    idk

--- public_html/script.js
+++ public_html/script.js
@@ -1,24 +1,24 @@
 $("document").ready(function() {
     $(".test").css("background-color", "blue");
     $('p:first').css({"background-color": "red", "color": "white"});
-    
-    $('p:last').css({"background-color": "black", "color": "white"});
 
+    $('p:last').css({"background-color": "black", "color": "white"});
 });
-$('document').ready(function(){
-$("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);    
-$('div').bind('mouse', mouseOverMe);
-$('h1').bind('click', mouseClick);
+
+$('document').ready(function() {
+    $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+    $('div').bind('mouse', mouseOverMe);
+    $('h1').bind('click', mouseClick);
 });
 
 function mouseOverMe() {
     $("p").html("blah");
 }
-function mouseOutMe () {
+function mouseOutMe() {
     $('h1').html("what's up?");
 }
 
-function mouseClick(){
+function mouseClick() {
     $('p').html('ugh');
 }
 $("window").resize(resizedWindow);
@@ -26,5 +26,17 @@ $("window").resize(resizedWindow);
 function resizedWindow()
 {
     $("#second").html("Window was resized W: " + $("window").width() + " H: + ");
-$(("window").height());
+    $(("window").height());
+}
+$("document").ready(function(){
+$('#hideLogo').bind('click', hideTheImage);
+        $('#showLogo').bind('click', showTheImage);
+        });
+        
+function hideTheImage(){
+    $('#logo').hide('explode',{}, 2500);
+}
+
+function showTheImage() {
+    $('#logo').show('fold', {}, 2500);
 }
